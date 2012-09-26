6938e72e1a5b05e4d379aaf6bb8417b849acea72
Anastasia Charushina <stasia-a@tut.by>
2012-09-26 17:00:13 +0300

    Remove folder static_pages

--- static_pages/app/helpers/static_pages_helper.rb
+++
@@ -1,5 +0,0 @@
-module StaticPagesHelper
-  def gravatar_helper(email)
-      "http://www.gravatar.com/avatar/#{ Digest::MD5.hexdigest(email) }?d=monsterid"
-  end
-end
--- static_pages/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StaticPages::Application.config.secret_token = 'b963417d23b70cf53a07339d0504696c245d1d03eff4330ded5f892b691c36ad1d24b45ed5183dfc92c10a66f4f236fceb7e3948f5f59c24d9db27642f7bcf84'
