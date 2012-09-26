f50eaae3e310285e1c83041b389c15dff7d5a3d4
Anastasia Charushina <stasia-a@tut.by>
2012-09-26 17:23:00 +0300

    Create seed and database for user email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,6 @@ class StaticPagesController < ApplicationController
   end
 
   def who_we_are
-    @emails =  %w( sergey.berdnikovich anastasia.charushina anton.dziga konstantin.ilchenko alexander.karmes stanislav.knyazev alexander.koshelapov yulia.oletskaya konstantin.savon anastasia.shaternik sergey.starikov viktor.voinilovich)
+    @users = User.all
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,9 @@ module ApplicationHelper
       content_tag(:li,link_to('About', about_path))
     end
 
-    #content_for :navigation do
-    #  content_tag(:li,link_to('Who We Are', who_we_are_path))
-    #end
+    content_for :navigation do
+      content_tag(:li,link_to('Who We Are', who_we_are_path))
+    end
     content_for(:navigation)
   end
 end
